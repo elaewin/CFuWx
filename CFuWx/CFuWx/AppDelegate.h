19e54bb6d3c5bf7b1be944f8b30cd3ed1bae23a5
Erica Winberry <elaewin@gmail.com>
2016-12-18 09:34:21 -0800

    Create new project

---
+++ CFuWx/CFuWx/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CFuWx
+//
+//  Created by Erica Winberry on 12/18/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CFuWx/CFuWx/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CFuWx
+//
+//  Created by Erica Winberry on 12/18/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
