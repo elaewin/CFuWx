0004aad92d3856023277d8c53474a48accfd3c6f
Erica Winberry <elaewin@gmail.com>
2016-12-18 09:42:12 -0800

    Add About, Forecast, Home, and Settings VCs

---
+++ CFuWx/CFuWx/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  CFuWx
+//
+//  Created by Erica Winberry on 12/18/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
