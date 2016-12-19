fdae20fafee96b16e80b4d9a4dc8733f2a7b198b
Jacob Ross Dobson <jacobdobson@jacob-dobsons-macbook-pro.local>
2016-12-19 15:54:21 -0800

    code should be in HomeViewController

--- CFuWx/CFuWx/HomeViewController.m
+++ CFuWx/CFuWx/HomeViewController.m
@@ -9,9 +9,12 @@
 #import "HomeViewController.h"
 #import "Altimiter.h"
 
+@import CoreLocation;
+
 @interface HomeViewController ()
 
 @property(strong, nonatomic)Altimiter *altimeter;
+@property(strong, nonatomic)CLLocationManager *locationManager;
 
 @end
 
@@ -21,7 +24,31 @@
     [super viewDidLoad];
     Altimiter *altimeter = [[Altimiter alloc]init];
     [self.altimeter getAltitudeChange];
+    
+    // Core Location request permission for user's current location
+    [self requestPermissions];
+    [self getCurrentLocationWithCoordinatesAndAltitude];
+}
 
+-(void)requestPermissions {
+    if ( ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied) || ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted) ) {
+        [self.locationManager requestWhenInUseAuthorization];
+    } else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [self setLocationManager:[[CLLocationManager alloc]init]];
+        [CLLocationManager locationServicesEnabled];
+    }
+}
+
+-(NSString *)getCurrentLocationWithCoordinatesAndAltitude {
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager startUpdatingLocation];
+    
+    NSString *currentLocation = [NSString stringWithFormat:@"latitude: %f longitude: %f altitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude, self.locationManager.location.altitude]; //putting coordinates(lat/long) and altitude as floats into string
+    
+    NSLog(@"%@", currentLocation);
+    
+    return currentLocation;
 }
 
 
