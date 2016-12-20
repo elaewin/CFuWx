811bdd9d8e0d3ec9526b95b8a9b83a633819c212
Erica Winberry <elaewin@gmail.com>
2016-12-20 14:10:31 -0800

    Create conversions class

---
+++ CFuWx/Conversions.h
@@ -0,0 +1,13 @@
+//
+//  Conversions.h
+//  CFuWx
+//
+//  Created by Erica Winberry on 12/20/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Conversions : NSObject
+
+@end
