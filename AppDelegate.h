7ba6a22231108c45c00d8f4869450cd565a15aad
awallraff <adam.wallraff@gmail.com>
2016-12-20 10:41:37 -0800

    Fixing Git Issues

--- AppDelegate.h
+++ AppDelegate.h
@@ -8,7 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
-@import Charts;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- CFuWx/HomeViewController.m
+++ CFuWx/HomeViewController.m
@@ -7,11 +7,16 @@
 //
 
 #import "HomeViewController.h"
-#import <CoreLocation/CoreLocation.h>
+#import "Altimeter.h"
+
+@import CoreLocation;
 
 @interface HomeViewController ()
 
+@property(strong, nonatomic)Altimeter *altimeter;
 @property(strong, nonatomic)CLLocationManager *locationManager;
+//@property(strong, nonatomic)CLGeocoder *gecoder;
+@property(strong, nonatomic)CLPlacemark *placemark;
 
 @end
 
@@ -19,6 +24,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.altimeter = [[Altimeter alloc]init];
+//    [self.altimeter getAltitudeChange];
     
     // Core Location request permission for user's current location
     [self requestPermissions];
@@ -44,6 +51,84 @@
     NSLog(@"%@", currentLocation);
     
     return currentLocation;
+}
+
+-(void)reverseGeocode {
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    CLLocation *location = self.locationManager.location;
+    
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        NSLog(@"Found placemarks: %@, error: %@", placemarks, error);
+        
+        if (error == nil && [placemarks count] > 0) {
+            _placemark = [placemarks lastObject];
+            
+            NSLog(@"%@", _placemark);
+            
+            return _placemark.locality;
+        } else {
+            return error.debugDescription;
+        }
+    }];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
 }
 
 
