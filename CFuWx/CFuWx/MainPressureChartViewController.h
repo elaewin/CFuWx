85126cced16a598a6054c248b54b2bc6f2863f63
Erica Winberry <elaewin@gmail.com>
2016-12-19 17:49:51 -0800

    Add charts framework, start building xib.

---
+++ CFuWx/CFuWx/MainPressureChartViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainPressureChartViewController.h
+//  CFuWx
+//
+//  Created by Erica Winberry on 12/19/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainPressureChartViewController : UIViewController
+
+@end
