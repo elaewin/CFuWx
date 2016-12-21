52bc76dc3ba0485acff2a032b3ea20ef531dbe1f
Corey Malek <coreymalek@Coreys-MacBook-Pro.local>
2016-12-21 10:09:26 -0800

    created weather class

---
+++ CFuWx/Weather.h
@@ -0,0 +1,13 @@
+//
+//  Weather.h
+//  CFuWx
+//
+//  Created by Corey Malek on 12/21/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Weather : NSObject
+
+@end
---
+++ CFuWx/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  CFuWx
+//
+//  Created by Corey Malek on 12/21/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
