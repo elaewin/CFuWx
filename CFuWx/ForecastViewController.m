c4fb424cc2affb47a733d2a523764b4699896b16
Jacob Dobson <jacobdobson@Jacob-Dobsons-MacBook-Pro.local>
2016-12-22 08:44:57 -0800

    added table view and messed with unsplash a bit

--- CFuWx/ForecastViewController.m
+++ CFuWx/ForecastViewController.m
@@ -10,28 +10,52 @@
 
 @interface ForecastViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+
 @end
 
 @implementation ForecastViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+
+
+/*
+ code for background images from storm collection via unsplash
+ 
+ -(UIImage *)getBackgroundImage {
+ //setting background to image grabbed from "Storms" collection in unsplash.com
+ NSURL *imageURL = [NSURL URLWithString:@"https://source.unsplash.com/collection/274155"];
+ 
+ dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+ NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+ 
+ dispatch_async(dispatch_get_main_queue(), ^{
+ // Update the UI
+ self.backgroundImageView.image = [UIImage imageWithData:imageData];
+ });
+ });
+ return self.backgroundImageView.image;
+ }
+ 
+ 
+ 
+ 
+ */
+
+
+
+
+/*
+ code for segue to this view controller(when forecast button in hamburger menu is pressed)
+ 
+ 
+ ForecastViewController *forecastViewController = [[ForecastViewController alloc]init];
+ [self presentViewController:forecastViewController animated:YES, completion:nil];
+ 
+ 
+ */
---
+++ HourlyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HourlyTableViewCell.h
+//  CFuWx
+//
+//  Created by Jacob Dobson on 12/21/16.
+//  Copyright © 2016 Erica Winberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HourlyTableViewCell : UITableViewCell
+
+@end
